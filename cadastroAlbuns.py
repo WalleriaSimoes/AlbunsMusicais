c86a5322501db8f122e1618d8fa8b25581dc86b6
Walléria Simões Correia <walleriasimoes@gmail.com>
2023-12-11 15:29:15 -0300

    Update cadastroAlbuns.py
    Adição da função salva_valores e inputs restantes (radiobutton e button)

--- cadastroAlbuns.py
+++ cadastroAlbuns.py
@@ -6,6 +6,15 @@ window = Tk()
 window.title("Cadastro de Álbuns")
 window.geometry('600x400')
 
+def salva_valores():
+    nomeAlbum = inputNomeAlbum.get()
+    dataLancamento = inputDataLancamento.get()
+    nomeBanda = inputNomeBanda.get()
+    albumLancamento = v0.get()
+
+    with open('Albuns_Cadastrados.txt', 'a') as albunsCadastrados:
+        albunsCadastrados.write(f"{nomeAlbum}|{dataLancamento}|{nomeBanda}|{albumLancamento}")
+
 labelNomeAlbum = Label(window, text= 'Nome do Álbum: ')
 labelNomeAlbum.grid(column= 0, row= 0, sticky= W)
 
@@ -26,5 +35,14 @@ inputNomeBanda.grid(column= 1, row= 2, sticky= E)
 
 labelAlbunLancamento = Label(window, text= 'Álbum lançamento: ')
 labelAlbunLancamento.grid(column= 0, row= 3, sticky= W)
+
+v0 = IntVar()
+r1 = Radiobutton(window, text= 'Sim', variable= v0, value= True)
+r1.grid(column= 1, row= 3, sticky= W)
+r2 = Radiobutton(window, text= 'Não', variable= v0, value= False)
+r2.grid(column= 1, row= 3, sticky= E)
+
+botao = Button(window, text= 'Cadastrar', command= salva_valores)
+botao.grid(column= 0, row= 4)
 
 window.mainloop()
